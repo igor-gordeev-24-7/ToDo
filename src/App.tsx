c3be1dadd8d7dd0de058612cea6ee8e114d6f0b1
igor-gordeev-24-7 <igor.gordeev.24.7@gmail.com>
2025-08-24 20:06:55 +0300

    Изменено расположения элементов, добавлены классы

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.module.scss';
 import { useTodos } from './hooks/useTodos';
 import type { Todo } from './types/Todo';
 
+import style from './App.module.scss'
+
 const App: React.FC = () => {
   const { todos, addTodo, toggleTodo, changeTextTodo, deleteTodo } = useTodos();
   const [input, setInput] = useState('');
@@ -40,14 +42,14 @@ const App: React.FC = () => {
   };
 
   return (
-    <div className="app">
-      <h1>Todo App</h1>
-      <form onSubmit={handleSubmit} className="add-form">
+    <div className={style.app}>
+      <h1 className={style.heading}>Todo App</h1>
+      <form onSubmit={handleSubmit} className={style.form}>
         <input
           value={input}
           onChange={(e) => setInput(e.target.value)}
           placeholder="Что нужно сделать"
-          className="input"
+          className={style.formInput}
         />
         <button type="submit" className="button">
           Добавить
@@ -58,14 +60,15 @@ const App: React.FC = () => {
         {todos.map((todo: Todo) => (
           <li key={todo.id} className="todo-item">
             {editingId === todo.id ? (
-              <>
+              // Режим редактирования
+              <div className={style.todoItem}>
                 <input
                   type="text"
                   value={editText}
                   onChange={(e) => setEditText(e.target.value)}
                   autoFocus
                   onKeyPress={(e) => e.key === 'Enter' && saveEdit()}
-                  className="edit-input"
+                  className={style.editInput}
                 />
                 <button onClick={saveEdit} className="save-btn">
                   Сохранить
@@ -73,31 +76,33 @@ const App: React.FC = () => {
                 <button onClick={cancelEdit} className="cancel-btn">
                   Отмена
                 </button>
-              </>
+              </div>
             ) : (
-              <>
-                <input
-                  type="checkbox"
-                  checked={todo.completed}
-                  onChange={() => toggleTodo(todo.id)}
-                  className="checkbox"
-                />
+              // Обычный режим
+              <div className={style.todoItem}>
                 <span
                   style={{
                     textDecoration: todo.completed ? 'line-through' : 'none',
                     cursor: 'pointer',
                   }}
                   onClick={() => startEditing(todo)}
+                  className={style.spanText}
                 >
                   {todo.text}
                 </span>
-                <button onClick={() => deleteTodo(todo.id)} className="delete-btn">
-                  Удалить
-                </button>
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={() => toggleTodo(todo.id)}
+                  className="checkbox"
+                />
                 <button onClick={() => startEditing(todo)} className="edit-btn">
                   Редактировать
                 </button>
-              </>
+                <button onClick={() => deleteTodo(todo.id)} className="delete-btn">
+                  Удалить
+                </button>
+              </div>
             )}
           </li>
         ))}
