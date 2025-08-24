aa1ed558cb05ddb295d9a58ad543dc36415f24bf
igor-gordeev-24-7 <igor.gordeev.24.7@gmail.com>
2025-08-24 19:34:44 +0300

    Испралена ошибка - todos не сохранялись в localStorage

--- src/hooks/useTodos.ts
+++ src/hooks/useTodos.ts
@@ -3,44 +3,40 @@ import type { Todo } from '../types/Todo';
 import { getTodos, saveTodos } from '../utils/localStorage';
 
 export const useTodos = () => {
-	const [todos, setTodos] = useState<Todo[]>([]);
+  const [todos, setTodos] = useState<Todo[]>(() => getTodos());
 
-	useEffect(() => {
-		setTodos(getTodos)
-	}, []);
+  useEffect(() => {
+    saveTodos(todos);
+  }, [todos]);
 
-	useEffect(() => {
-		saveTodos(todos);
-	}, [todos]);
+  const addTodo = (text: string) => {
+    const newTodo: Todo = {
+      id: Date.now(),
+      text,
+      completed: false,
+    };
+    setTodos((prev) => [...prev, newTodo]);
+  };
 
-	const addTodo = (text: string) => {
-		const newTodo: Todo = {
-			id: Date.now(),
-			text,
-			completed: false,
-		};
-		setTodos((prev) => [...prev, newTodo]);
-	}
+  const toggleTodo = (id: number) => {
+    setTodos((prev) =>
+      prev.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
 
-	const toggleTodo = (id: number) => {
-		setTodos((prev) => 
-			prev.map((todo) => 
-				todo.id === id ? { ...todo, completed: !todo.completed} : todo
-			)
-		);
-	}
+  const changeTextTodo = (id: number, modifiedText: string) => {
+    setTodos((prev) =>
+      prev.map((todo) =>
+        todo.id === id ? { ...todo, text: modifiedText } : todo
+      )
+    );
+  };
 
-	const changeTextTodo = (id: number, modifiedText: string) => {
-		setTodos((prev) =>
-			prev.map((todo) => 
-				todo.id === id ? {...todo, text: modifiedText} : todo
-			)
-		);
-	}
-	
-	const deleteTodo = (id: number) => {
-		setTodos((prev) => prev.filter((todo) => todo.id !== id));
-	}
+  const deleteTodo = (id: number) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
 
-	return { todos, addTodo, toggleTodo, changeTextTodo, deleteTodo };
-}
+  return { todos, addTodo, toggleTodo, changeTextTodo, deleteTodo };
+};
