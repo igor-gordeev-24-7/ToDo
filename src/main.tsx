850f2c8e155a55cebf3240fc949e9a6b152e54cd
igor-gordeev-24-7 <igor.gordeev.24.7@gmail.com>
2025-08-26 18:03:25 +0300

    Корректировки под контекст и смену темы

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.scss'
-import App from './App.tsx'
+// src/main.tsx или index.tsx
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
+import App from './App';
+import { ThemeProvider } from './context/ThemeProvider';
+import './index.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
-)
+);
